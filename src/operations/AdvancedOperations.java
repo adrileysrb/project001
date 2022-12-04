cf69e0add002fb95c02971cd159204b3fecef1ae
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2022-12-04 19:23:53 -0300

    Operations has been defined.

---
+++ src/operations/AdvancedOperations.java
@@ -0,0 +1,19 @@
+package operations;
+
+public class AdvancedOperations {
+
+    //TODO Implement divide operation
+    public Double divide (){
+       return 0.0;
+    }
+
+    //TODO Implement rooting operation
+    public Double rooting (){
+        return 0.0;
+    }
+
+    //TODO Implement potentiation operation
+    public Double potentiation (){
+        return 0.0;
+    }
+}
---
+++ src/operations/BasicOperations.java
@@ -0,0 +1,24 @@
+package operations;
+
+public class BasicOperations {
+    //TODO Implement sum operation
+    public Double sum(Double a, Double b){
+        return 0.0;
+    }
+
+    //TODO Implement subtract operation
+    public Double subtract(Double a, Double b){
+        return 0.0;
+    }
+
+    //TODO Implement divide operation
+    public Double divide(Double a, Double b){
+        return 0.0;
+    }
+
+    //TODO Implement multiply operation
+
+    public Double multiply(Double a, Double b){
+        return a * b;
+    }
+}
