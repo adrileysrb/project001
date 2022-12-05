a4160cc8c371de67e2fa35fa30dda0a4154ba4a8
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2022-12-04 21:07:06 -0300

    Add unit test for AdvancedOperations

---
+++ src/test/AdvancedOperationsTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdvancedOperationsTest {
+
+    @org.junit.jupiter.api.Test
+    void restDivision() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void potentiation() {
+    }
+}
