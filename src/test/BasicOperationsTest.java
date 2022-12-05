9fe3f525ae874152e4eb64b3f4a20edee284d21f
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2022-12-05 09:57:07 -0300

    BasicOperation unit tests has been implemented.

--- src/test/BasicOperationsTest.java
+++ src/test/BasicOperationsTest.java
@@ -1,22 +1,40 @@
 package test;
 
+import exceptions.DivisionByZero;
+import operations.BasicOperations;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasicOperationsTest {
 
+    private static BasicOperations basicOperations;
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpBeforeClass() {
+        basicOperations = new BasicOperations();
+    }
+
     @org.junit.jupiter.api.Test
     void sum() {
+        Double result = basicOperations.sum(1.0, 3.0);
+        assertEquals(4.0, result);
     }
 
     @org.junit.jupiter.api.Test
     void subtract() {
+        Double result = basicOperations.subtract(5.0, 3.0);
+        assertEquals(2.0, result);
     }
 
     @org.junit.jupiter.api.Test
-    void divide() {
+    void divide() throws DivisionByZero {
+        Double result = basicOperations.divide(10.0, 2.0);
+        assertEquals(5.0, result);
     }
 
     @org.junit.jupiter.api.Test
     void multiply() {
+        Double result = basicOperations.multiply(2.0, 2.0);
+        assertEquals(4.0, result);
     }
 }
