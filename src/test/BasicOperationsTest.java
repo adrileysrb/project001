8dcb69e1ed9abe283810a8b6a065e2070e867385
Adriley Samuel Ribeiro Barbosa <adrileysrb@gmail.com>
2022-12-04 21:11:10 -0300

    Add unit test BasicOperations.

---
+++ src/test/BasicOperationsTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasicOperationsTest {
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+}
